caec2e4cc7c3f32b62db2e7fdb1da9326cbfbff6
SashaLeshiy <zomlesh@yandex.ru>
2023-08-04 11:35:34 +0300

    ready

---
+++ src/components/BlockRightTop.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const getItem = computed(() => store.getters.getRightTop)
+
+</script>
+<template>
+    <div class="block-right-top">
+        <div v-if="getItem" class="block-right-top__item">
+            {{ getItem.name }}
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.block-right-top {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #000000;
+}
+</style>
